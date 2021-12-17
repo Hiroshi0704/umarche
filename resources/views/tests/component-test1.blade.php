0398810e2bb44ad6b3ec617c397bdb74232b3101
Hiroshi0704 <h.watanabe.3495@gmail.com>
2021-12-17 17:49:52 +0900

    add component attributes

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,11 @@
 <x-tests.app>
-    <x-slot name="header">
-        header1
-    </x-slot>
-    component-test1
+  <x-slot name="header">
+    header1
+  </x-slot>
+  component-test1
+
+  <x-tests.card
+    title="タイトル"
+    content="本文"
+  />
 </x-tests.app>
