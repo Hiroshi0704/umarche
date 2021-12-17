01a0907e6cab2802afe80fcdf0bc609a9299c8e1
Hiroshi0704 <h.watanabe.3495@gmail.com>
2021-12-17 18:14:01 +0900

    add component attributes bug

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,5 +5,6 @@
     component-test1
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
-    <x-tests.card title="タイトル" />
+    <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
